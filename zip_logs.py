c765ef97a82960fe026f018a6399af5051e29f93
peteremeka753-maker <peteremeka753@gmail.com>
2026-03-27 17:14:27 +0100

    Delete zip_logs.py

--- zip_logs.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import os
-
-def zip_logs():
-    with zipfile.ZipFile("backup.zip", "w") as z:
-        for root, dirs, files in os.walk("data"):
-            for file in files:
-                z.write(os.path.join(root, file))
